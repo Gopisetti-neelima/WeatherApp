d31619286648fde82c4bea9e020ce48c3a330331
Neelima Gopisetti <36102261+Gopisetti-neelima@users.noreply.github.com>
2023-06-23 01:21:08 +0530

    Styling
    Results are shown properly. Background image changes based on weather ex: Rain, Snow etc

--- app.js
+++ app.js
@@ -5,15 +5,43 @@ function getTemp(city){
 	{
 		fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
 	.then(res=>res.json())
-	.then(data=> data.main)
-	.then(temp=>{
-		console.log(temp);
-		let x=JSON.stringify(temp);
-		console.log(x);
-		document.getElementById('res').innerHTML=x;
+	.then(d=>{
+		console.log(d);
+		temperature = FarenheitToCelcius(d.main["temp"]);
+		document.getElementById('t').innerHTML = temperature + "°C";
+		document.getElementById('fl').innerHTML = FarenheitToCelcius(d.main["feels_like"]) + "°C";
+		document.getElementById('lt').innerHTML = FarenheitToCelcius(d.main["temp_min"]) + "°C";
+		document.getElementById('ht').innerHTML = FarenheitToCelcius(d.main["temp_max"]) + "°C";
+		document.getElementById('hum').innerHTML = d.main["humidity"] + "%";
+		console.log(d);
+
+		wallpaper = "assets/day.jpg";
+		if(temperature < 20){
+			wallpaper = "assets/day-winter.jpg";
+		}
+
+		wtype = d.weather[0]["main"];
+		if(wtype == "Rain"){
+			wallpaper = "assets/rain.jpg";
+		}
+		else if(wtype == "Snow")
+			wallpaper = "assets/day-winter.jpg";
+		else if(wtype == "Spring")
+			wallpaper = "assets/day-spring.jpg";
+			
+		document.body.style.backgroundImage = 'url("' + wallpaper + '")';
+		document.body.style.backgroundSize = "cover";
+		document.body.style.backgroundPosition = "center";
+
 	})
 	.catch(error=>console.log(error));
 	}
+}
+
+function FarenheitToCelcius(data){
+	data = parseInt(data);
+	// res = (data-32)*5.0/9.0;
+	return parseInt(data - 273.15) ;
 }
 
 function getData(){
